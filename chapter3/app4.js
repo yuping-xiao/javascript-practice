66babb130d1928e08e5695f6d555e2887661f548
yuping-xiao <yupingxiao94@gmail.com>
2020-06-09 23:57:14 +0900

    modified rock-paper-scissors

--- chapter3/app4.js
+++ chapter3/app4.js
@@ -1,10 +1,10 @@
 var user_hand = prompt('じゃんけんの手をグー、チョキ、パーの中から選んでください。');
 
-var js_hand = geJShand();
+var js_hand = getJShand();
 
 var judge = winLose(user_hand, js_hand);
 
-alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + 'です。\n結果は' + judge + 'です。' );
+alert( 'あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。' );
 
 function getJShand(){
 	var js_hand_num = Math.floor( Math.random() * 3 );
@@ -25,33 +25,19 @@ function winLose(user, js){
 	var winLoseStr;
 
 	if(user == "グー"){
-		if(js == "グー"){
-			winLoseStr = "あいこ";
-		}else if(js == "チョキ"){
-			winLoseStr = "勝ち";
-		}else if(js == "パー"){
-			winLoseStr = "負け";
-		}
+		if(js == "グー"){ winLoseStr = "あいこ"; }
+		else if(js == "チョキ"){ winLoseStr = "勝ち";}
+		else if(js == "パー"){ winLoseStr = "負け"; }
+
 	}else if(user == "チョキ"){
-		if(js == "グー"){
-			winLoseStr = "負け";
-		}else if(js == "チョキ"){
-			sinLoseStr = "あいこ";
-		}else if(js == "パー"){
-			winLoseStr = "勝ち";
-		}
+		if(js == "グー"){ winLoseStr = "負け"; }
+		else if(js == "チョキ"){ winLoseStr = "あいこ"; }
+		else if(js == "パー"){ winLoseStr = "勝ち"; }
+
 	}else if(user == "パー"){
-		if(js == "グー"){
-			winLoseStr = "勝ち";
-		}
-	}else if(user == "パー"){
-		if(js == "グー"){
-			winLoseStr = "勝ち";
-		}else if(js == "チョキ"){
-			winLoseStr = "負け";
-		}else if(js == "パー"){
-			winLoseStr = "あいこ";
-		}
+		if(js == "グー"){ winLoseStr = "勝ち"; }
+		else if(js == "チョキ"){ winLoseStr = "負け"; }
+		else if(js == "パー"){ winLoseStr = "あいこ"; }
 	}
 
 	return winLoseStr;
