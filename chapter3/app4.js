b43d3a02feebd29ec570c71dacbc64d25497f8a3
yuping-xiao <yupingxiao94@gmail.com>
2020-06-12 23:38:57 +0900

    finished rock-paper-scissors function

--- chapter3/app4.js
+++ chapter3/app4.js
@@ -1,17 +1,20 @@
 var user_hand = prompt('じゃんけんの手をグー、チョキ、パーの中から選んでください。');
-while ((user_hand != "グー") && (user_hand != "チョキ") && (user_hand != "パー") && (user_hand != "null")){
-	alert('グー、チョキ、パーのいずれかを選んでください');
-	user_hand = prompt('グー、チョキ、パーを選んでください');
+while ((user_hand != "グー") && (user_hand != "チョキ") && (user_hand != "パー") && (user_hand != null)){
+	alert('グー、チョキ、パーのいずれかを入力してください');
+	user_hand = prompt('グー、チョキ、パーの中から手を選んでください');
 }
 
 var js_hand = getJShand();
+var judge = winLose(user_hand,js_hand);
 
-var judge = winLose(user_hand, js_hand);
-
-alert( 'あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。' );
+if (user_hand != null) {
+	alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだは' + js_hand + 'です。\n結果は' + judge + 'です。');
+} else {
+	alert("またチャレンジしてね！")
+}
 
 function getJShand(){
-	var js_hand_num = Math.floor( Math.random() * 3 );
+	var js_hand_num = Math.floor(Math.random() * 3);
 	var hand_name;
 
 	if(js_hand_num == 0){
@@ -25,7 +28,7 @@ function getJShand(){
 	return hand_name;
 }
 
-function winLose(user, js){
+function winLose(user,js){
 	var winLoseStr;
 
 	if(user == "グー"){
